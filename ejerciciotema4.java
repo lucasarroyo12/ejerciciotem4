ab399fc018a1b7449e194fc6afb18fd86e0e16f6
Lucas Arroyo <lucasarroyo12@gmail.com>
2022-08-18 23:48:02 -0300

    ejercicios tema 4

---
+++ ejerciciotema4.java
@@ -0,0 +1,41 @@
+/* apartado 1*/
+if(numeroif > 0){
+    System.out.println("los numeros son positivos");
+} else {
+    System.out.println("los numeros son negativos")
+}
+
+/* apartado 2*/
+while (numeroWhile < 3 ){
+    numeroWhile ++;
+    System.out.println(numeroWhile);
+}
+
+/* apartado 3*/
+do{
+    numeroWhile ++
+    System.out.println(numeroWhile);
+} while(numeroWhile < 3);
+
+/* apartado 4*/
+for(int numeroFor=0 ; numeroFor<=3 ; numeroFor ++ ){
+    System.out.println(numeroFor);
+}
+
+/* apartado 5*/
+int estacion;
+switch (estacion){
+    case1: estacion = 1;
+        System.out.println("la estacion es invierno");
+    case2: estacion = 2;
+        System.out.println("la estacion es otoño");
+    case3: estacion = 3;
+        System.out.println("la estacion es privamera");
+    case4: estacion = 4;
+        System.out.println("la estacion es verano");
+}
+
+    System.out.println("El valor estacion no corresponde a ninguna");
+
+
+
